0cea48e4edc9a8ab528ae8aae7d328b812e990f4
Nargiza Konushalieva <64344753+Lebedok@users.noreply.github.com>
2020-11-18 10:06:03 -0600

    Add files via upload

---
+++ buy_mil.js
@@ -0,0 +1,17 @@
+function getMilk(money, costPerBottle) {
+    return calcChange (money, costPerBottle);
+    }
+    
+    function calcBottles(startingMoney, costPerBottle);
+    var numberOfBottles = Math.floor(startingMoney / coastPerBottle);
+    return numberOfBottles;
+    
+    
+    
+    function calcChange(startingAmount, coastPerBottle) {
+    var change = startingAmount % costPerBottle;
+    return change;
+    
+    }
+    
+    Console.log("Hello master, here is your" + getMilk(5, 1,5) + "change");
